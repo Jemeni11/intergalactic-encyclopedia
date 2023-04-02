6c306f333d1e04623cb706d4c38440902b22bd08
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-02 12:41:47 +0100

    feat(_layout.tsx): Added fonts
    BREAKING CHANGE:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,51 @@
+import { useCallback } from "react";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { colors } from "../helpers/colors";
+import {
+  useFonts,
+  Barlow_400Regular,
+  Barlow_400Regular_Italic,
+  Barlow_500Medium,
+  Barlow_500Medium_Italic,
+  Barlow_600SemiBold,
+  Barlow_600SemiBold_Italic,
+  Barlow_700Bold,
+  Barlow_700Bold_Italic,
+  Barlow_800ExtraBold,
+  Barlow_800ExtraBold_Italic,
+  Barlow_900Black,
+  Barlow_900Black_Italic,
+} from "@expo-google-fonts/barlow";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  let [fontsLoaded] = useFonts({
+    Barlow_400Regular,
+    Barlow_400Regular_Italic,
+    Barlow_500Medium,
+    Barlow_500Medium_Italic,
+    Barlow_600SemiBold,
+    Barlow_600SemiBold_Italic,
+    Barlow_700Bold,
+    Barlow_700Bold_Italic,
+    Barlow_800ExtraBold,
+    Barlow_800ExtraBold_Italic,
+    Barlow_900Black,
+    Barlow_900Black_Italic,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <SafeAreaProvider>
       <Stack
@@ -11,6 +54,9 @@ const Layout = () => {
           headerShadowVisible: false,
           headerTitleAlign: "center",
           headerBackTitleVisible: false,
+          headerTitleStyle: {
+            fontFamily: "Barlow_700Bold",
+          },
           headerStyle: {
             backgroundColor: colors.secondary,
           },
