f22041bff355ae6cdbabb1b67513ff5d01235f12
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-12-01 17:30:48 +0100

    Refactor PageLayout component

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,14 +1,8 @@
 import { StyleSheet, View, StatusBar } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import type { LayoutChangeEvent } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const PageLayout = ({
-  children,
-  onLayout,
-}: {
-  children: React.ReactNode;
-  onLayout?: ((event: LayoutChangeEvent) => void) | undefined;
-}) => {
+const PageLayout = ({ children, onLayout }: { children: React.ReactNode; onLayout?: ((event: LayoutChangeEvent) => void) | undefined }) => {
   const insets = useSafeAreaInsets();
   return (
     <View
